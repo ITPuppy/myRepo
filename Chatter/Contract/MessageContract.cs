cafc3cde3741d66120c9296c0b3a4e93967c314d
ITPuppy <caifengchen.cn@gmail.com>
2012-12-29 14:01:26 +0800

    new solution

---
+++ Chatter/Contract/MessageContract.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+namespace Contract
+{
+    [DataContract]
+    class MessageContract
+    {
+        [DataMember]
+        private string head;
+        [DataMember]
+        private string body;
+        [DataMember]
+        private string tail;
+
+        public string Tail
+        {
+            get { return tail; }
+            set { tail = value; }
+        }
+
+        public string Body
+        {
+            get { return body; }
+            set { body = value; }
+        }
+
+        public string Head
+        {
+            get { return head; }
+            set { head = value; }
+        }
+
+    }
+}
